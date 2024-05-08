0059b2d644940bd458b315ec97b63c7029d46f6b
tndumarevskaya <dumtanya2009@gmail.com>
2024-05-08 12:30:50 +0300

    made adaptive header and infoblock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import InfoBlock from './components/InfoBlock/InfoBlock';
-import Location from './components/Location/Location';
-import Schedule from './components/Schedule/Schedule';
-import DressCode from './components/DressCode/DressCode';
-import Questions from './components/Questions/Questions';
-import Footer from './components/Footer/Footer';
+// import Location from './components/Location/Location';
+// import Schedule from './components/Schedule/Schedule';
+// import DressCode from './components/DressCode/DressCode';
+// import Questions from './components/Questions/Questions';
+// import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
